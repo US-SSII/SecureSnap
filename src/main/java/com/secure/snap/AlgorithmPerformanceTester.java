8b50fdf89085d3466a3a47fc6c3608f5964f48b5
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-13 13:16:05 +0100

    refactor: Separated in multiples methods

--- src/main/java/com/secure/snap/AlgorithmPerformanceTester.java
+++ src/main/java/com/secure/snap/AlgorithmPerformanceTester.java
@@ -7,12 +7,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class tests the performance of various encryption algorithms.
+ */
 public class AlgorithmPerformanceTester {
 
+    /**
+     * Main method to run the algorithm performance tests.
+     * @param args Command-line arguments (not used)
+     */
     public static void main(String[] args) {
         List<GenericSecureSnap> secureSnaps = new ArrayList<>();
         try {
-            secureSnaps.add(new SecureSnap()); // AES con GCM y 256 bits
+            secureSnaps.add(new SecureSnap()); // AES with GCM and 256 bits
             secureSnaps.add(new AllSecureSnap("Blowfish", "ECB", "PKCS5Padding", 128)); // Blowfish
             secureSnaps.add(new AllSecureSnap("SEED", "ECB", "PKCS5Padding", 128)); // SEED
             secureSnaps.add(new AllSecureSnap("DESede", "ECB", "PKCS5Padding", 168)); // Triple DES
@@ -37,10 +44,8 @@ public class AlgorithmPerformanceTester {
 
         for (GenericSecureSnap snap : secureSnaps) {
             try {
-                long totalEncryptionTime = testEncryptionTime(snap, filesToProcess);
-                long totalDecryptionTime = testDecryptionTime(snap, filesToProcess);
-                long avgEncryptionTime = totalEncryptionTime / filesToProcess.size();
-                long avgDecryptionTime = totalDecryptionTime / filesToProcess.size();
+                long avgEncryptionTime = testEncryptionTime(snap, filesToProcess);
+                long avgDecryptionTime = testDecryptionTime(snap, filesToProcess);
                 avgEncryptionTimes.put(snap, avgEncryptionTime);
                 avgDecryptionTimes.put(snap, avgDecryptionTime);
             } catch (Exception e) {
@@ -49,37 +54,88 @@ public class AlgorithmPerformanceTester {
         }
 
         // Output algorithm performances
-        Table.Builder table = new Table.Builder().withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
+        printAlgorithmPerformance(secureSnaps, avgEncryptionTimes, avgDecryptionTimes);
+    }
+
+    /**
+     * Tests the encryption time for a given encryption algorithm.
+     * @param snap The encryption algorithm to test
+     * @param files The list of files to encrypt
+     * @return The average encryption time in milliseconds
+     * @throws Exception If an error occurs during encryption
+     */
+    public static long testEncryptionTime(GenericSecureSnap snap, List<String> files) throws Exception {
+        long totalEncryptionTime = 0;
+        for (String file : files) {
+            totalEncryptionTime += measureEncryptionTime(snap, file);
+        }
+        return totalEncryptionTime / files.size();
+    }
+
+    /**
+     * Measures the encryption time for a single file.
+     * @param snap The encryption algorithm to use
+     * @param file The file to encrypt
+     * @return The encryption time in milliseconds
+     * @throws Exception If an error occurs during encryption
+     */
+    public static long measureEncryptionTime(GenericSecureSnap snap, String file) throws Exception {
+        long startTime = System.currentTimeMillis();
+        snap.encrypt(file, file + ".enc");
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    /**
+     * Tests the decryption time for a given encryption algorithm.
+     * @param snap The encryption algorithm to test
+     * @param files The list of files to decrypt
+     * @return The average decryption time in milliseconds
+     * @throws Exception If an error occurs during decryption
+     */
+    public static long testDecryptionTime(GenericSecureSnap snap, List<String> files) throws Exception {
+        long totalDecryptionTime = 0;
+        for (String file : files) {
+            totalDecryptionTime += measureDecryptionTime(snap, file);
+        }
+        return totalDecryptionTime / files.size();
+    }
+
+    /**
+     * Measures the decryption time for a single file.
+     * @param snap The encryption algorithm to use
+     * @param file The file to decrypt
+     * @return The decryption time in milliseconds
+     * @throws Exception If an error occurs during decryption
+     */
+    public static long measureDecryptionTime(GenericSecureSnap snap, String file) throws Exception {
+        String extension = file.substring(file.lastIndexOf('.'));
+        long startTime = System.currentTimeMillis();
+        snap.decrypt(file + ".enc", file.replace(extension, "_decrypted" + extension));
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    /**
+     * Prints the algorithm performance in a tabular format.
+     * @param secureSnaps The list of encryption algorithms
+     * @param avgEncryptionTimes The average encryption times
+     * @param avgDecryptionTimes The average decryption times
+     */
+    public static void printAlgorithmPerformance(List<GenericSecureSnap> secureSnaps,
+                                                 Map<GenericSecureSnap, Long> avgEncryptionTimes,
+                                                 Map<GenericSecureSnap, Long> avgDecryptionTimes) {
+        Table.Builder table = new Table.Builder()
+                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
                 .addRow("Algorithm", "Average Encryption time (milliseconds)", "Average Decryption time (milliseconds)");
         for (GenericSecureSnap snap : secureSnaps) {
             table.addRow(snap.getAlgorithm(), avgEncryptionTimes.get(snap).toString(), avgDecryptionTimes.get(snap).toString());
         }
         System.out.println(table.build());
     }
-
-    public static long testEncryptionTime(GenericSecureSnap snap, List<String> files) throws Exception {
-        long totalEncryptionTime = 0;
-        for (String file : files) {
-            long startTime = System.currentTimeMillis();
-            snap.encrypt(file, file + ".enc");
-            long endTime = System.currentTimeMillis();
-            totalEncryptionTime += (endTime - startTime);
-        }
-        return totalEncryptionTime;
-    }
-
-    public static long testDecryptionTime(GenericSecureSnap snap, List<String> files) throws Exception {
-        long totalDecryptionTime = 0;
-        for (String file : files) {
-            long startTime = System.currentTimeMillis();
-            String extension = file.substring(file.lastIndexOf('.'));
-            snap.decrypt(file + ".enc", file.replace(extension, "_decrypted" + extension));
-            long endTime = System.currentTimeMillis();
-            totalDecryptionTime += (endTime - startTime);
-        }
-        return totalDecryptionTime;
-    }
 }
+
+
 
 
 
